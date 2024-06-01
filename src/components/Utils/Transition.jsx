54d90c2ba60dd04096f29f92c3a69361bfc413ef
rbhagat32 <raghavbhagat32@gmail.com>
2024-06-01 14:09:15 +0530

    border added in loader

--- src/components/Utils/Transition.jsx
+++ src/components/Utils/Transition.jsx
@@ -7,20 +7,22 @@ const Transition = (Component) => {
       <Component />
 
       <motion.div
-        className="flex justify-center items-center fixed top-0 left-0 w-full h-screen bg-black z-40 origin-top"
+        className="fixed top-0 left-0 w-full h-screen bg-black z-40 origin-top p-24"
         initial={{ y: 0 }}
         animate={{ y: "-100%" }}
         transition={{ duration: 2, ease: [0.22, 1, 0.36, 1], delay: 2 }}
       >
-        <h1 className="z-50 text-8xl font-bold">
-          Refokus
-          <motion.span
-            animate={{ color: "rgb(34 197 94)" }}
-            transition={{ delay: 1, duration: 0 }}
-          >
-            .
-          </motion.span>
-        </h1>
+        <div className="flex justify-center items-center w-full h-full border-y border-zinc-700">
+          <h1 className="z-50 text-8xl font-bold">
+            Refokus
+            <motion.span
+              animate={{ color: "rgb(34 197 94)" }}
+              transition={{ delay: 1, duration: 0 }}
+            >
+              .
+            </motion.span>
+          </h1>
+        </div>
       </motion.div>
     </>
   );
