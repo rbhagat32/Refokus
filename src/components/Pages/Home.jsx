746b885653cf8ea207765855a810c440089048ac
rbhagat32 <raghavbhagat32@gmail.com>
2024-06-17 22:04:23 +0530

    home page

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Transition from "../Utils/Transition";
+import Scene from "../Home/Scene";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container mx-auto">
+      <Scene />
+    </div>
+  );
 };
 
 export default Transition(Home);
